526b400a912719a9b72cb545e6744c5cac6dc5e4
harutosata0206-create <haruto.sata0206@gmail.com>
2026-03-13 21:06:35 +0900

    remove : fileタブ内のページ設定の削除

--- src/App.tsx
+++ src/App.tsx
@@ -2845,10 +2845,6 @@ function App() {
                     <span className="menu-shortcut">Ctrl+Alt+S</span>
                   </button>
                   <div className="menu-divider" />
-                  <button type="button" className="menu-item disabled" aria-disabled="true">
-                    <span>ページ設定</span>
-                  </button>
-                  <div className="menu-divider" />
                   <button
                     type="button"
                     className="menu-item toggle"
